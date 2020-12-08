3aa15543235c0000ab11703968b33389f9e9fab9
Bobo <bobodraganov@gmail.com>
2020-12-08 21:25:19 +0200

    Removed `import * as ReactDOM`

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
-import * as ReactDOM from 'react-dom';
+import { render } from 'react-dom';
 import { AppComponent } from './components/app/app.component';
 import { Provider } from 'react-redux';
 import store from './store/store';
 import { BrowserRouter } from 'react-router-dom';
 
-ReactDOM.render(
+render(
     <Provider store={store}>
         <BrowserRouter>
             <AppComponent />
